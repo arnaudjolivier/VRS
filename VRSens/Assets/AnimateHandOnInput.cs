6468904e141941a2a77b55c7ff709602a1bf0584
arnaudjolivier <81567917+arnaudjolivier@users.noreply.github.com>
2023-02-23 10:12:00 +0100

    hand

---
+++ VRSens/Assets/AnimateHandOnInput.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    public InputActionProperty pinchAnimation;
+    public InputActionProperty gripAnimation;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = pinchAnimation.action.ReadValue<float>();
+        animator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAnimation.action.ReadValue<float>();
+        animator.SetFloat("Grip", gripValue);
+    }
+}
